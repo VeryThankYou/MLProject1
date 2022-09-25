f78cfac23fc71add5849d1376ccb15608a641cb4
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2022-09-25 21:18:15 +0200

    Check for nan

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ raw_data = pddata.values
 cols = len(pddata.columns)
 attributeNames = np.asarray(pddata.columns)
 
+
 X = np.asarray(raw_data)
 
 X = np.delete(X, range(0, 10), 1)
 X = np.delete(X, [12, 13, 14, -1], 1)
 print(X)
-print(np.nan)
+print(pddata.isnull().sum())
+print(type(X[0, 0]))
+print(X[0, 0])
 
-print(np.where(X != np.nan and (type(X) != int or float)))
-
-print(np.count_nonzero(X, 0))
+count = (X == np.nan).sum()
+print(count)
