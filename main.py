936435be2d05a25b23a1fbf3060dda701c7de1bc
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2022-09-27 16:35:53 +0200

    data selection

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import numpy as np
 import pandas as pd
 
 pddata = pd.read_csv("anage.csv")
-raw_data = pddata.values
-cols = len(pddata.columns)
-attributeNames = np.asarray(pddata.columns)
-
-
-X = np.asarray(raw_data)
-print(X[1])
-X = np.delete(X, [0, 1, 2, 3, 4, 5, 6, 7, 8, 22, 23, 24, 25, 26, 27, 29, 30], 1)
-
-print(X[1])
-
+l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 15, 17, 19, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+l.reverse()
+for e in l:
+    pddata = pddata.drop(pddata.columns[e], axis=1)
+print(pddata)
 print(pddata.isnull().sum())
-print(type(X[0, 0]))
-print(X[0, 0])
-np.count_nonzero(np.isnan(X))
-count = (X == np.nan).sum()
-print(count)
+
+
+
+X = pddata.to_numpy()
+
+
+# print(pddata.isnull().sum())
+# print(type(X[0, 0]))
+# print(X[0, 0])
+X = X[~np.isnan(X).any(axis = 1)]
+print(X.shape)
