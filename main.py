2966fc786a394a4cdee47f115a0be602dc0602dc
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2022-09-27 15:53:21 +0200

    cut more

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ attributeNames = np.asarray(pddata.columns)
 
 
 X = np.asarray(raw_data)
+print(X[1])
+X = np.delete(X, [0, 1, 2, 3, 4, 5, 6, 7, 8, 22, 23, 24, 25, 26, 27, 29, 30], 1)
 
-X = np.delete(X, range(0, 10), 1)
-X = np.delete(X, [12, 13, 14, -1], 1)
-print(X)
+print(X[1])
+
 print(pddata.isnull().sum())
 print(type(X[0, 0]))
 print(X[0, 0])
-
+np.count_nonzero(np.isnan(X))
 count = (X == np.nan).sum()
 print(count)
