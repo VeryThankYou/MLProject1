0c244f87a85b8b08ddcfdb6fd5ef5260bde082f4
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2022-10-02 19:44:56 +0200

    PCAPlots

--- pca.py
+++ pca.py
@@ -1,8 +1,7 @@
 from dataload import *
 from scipy.linalg import svd
 import numpy as np
-import matplotlib.pyplot as plt
-
+from matplotlib.pyplot import (figure, subplot, boxplot, title, xticks, ylim, show, plot, xlabel, ylabel, yticks, legend, grid, bar)
 
 # Subtract mean value from data
 Y = X - np.ones((N,1))*X.mean(axis=0)
@@ -16,13 +15,67 @@ rho = (S*S) / (S*S).sum()
 threshold = 0.9
 print(S)
 # Plot variance explained
-plt.figure()
-plt.plot(range(1,len(rho)+1),rho,'x-')
-plt.plot(range(1,len(rho)+1),np.cumsum(rho),'o-')
-plt.plot([1,len(rho)],[threshold, threshold],'k--')
-plt.title('Variance explained by principal components')
-plt.xlabel('Principal component')
-plt.ylabel('Variance explained')
-plt.legend(['Individual','Cumulative','Threshold'])
-plt.grid()
-plt.show()
+figure()
+plot(range(1,len(rho)+1),rho,'x-')
+plot(range(1,len(rho)+1),np.cumsum(rho),'o-')
+plot([1,len(rho)],[threshold, threshold],'k--')
+title('Variance explained by principal components')
+xlabel('Principal component')
+ylabel('Variance explained')
+legend(['Individual','Cumulative','Threshold'])
+grid()
+show()
+
+
+# Subtract mean value from data
+Y = X - np.ones((N,1))*X.mean(0)
+
+# PCA by computing SVD of Y
+U,S,Vh = svd(Y,full_matrices=False)
+# scipy.linalg.svd returns "Vh", which is the Hermitian (transpose)
+# of the vector V. So, for us to obtain the correct V, we transpose:
+V = Vh.T    
+
+# Project the centered data onto principal component space
+Z = Y @ V
+
+# Indices of the principal components to be plotted
+i = 0
+j = 1
+
+# Plot PCA of the data
+f = figure()
+title('AnAge data: PCA')
+#Z = array(Z)
+for c in range(C):
+    # select indices belonging to class c:
+    class_mask = y==c
+    plot(Z[class_mask,i], Z[class_mask,j], 'o', alpha=.5)
+legend(classNames)
+xlabel('PC{0}'.format(i+1))
+ylabel('PC{0}'.format(j+1))
+
+# Output result to screen
+show()
+
+Y = X - np.ones((N,1))*X.mean(0)
+U,S,Vh = svd(Y,full_matrices=False)
+V=Vh.T
+N,M = X.shape
+
+# We saw in 2.1.3 that the first 3 components explaiend more than 90
+# percent of the variance. Let's look at their coefficients:
+pcs = [0,1,2]
+legendStrs = ['PC'+str(e+1) for e in pcs]
+c = ['r','g','b']
+bw = .2
+r = np.arange(1,M+1)
+for i in pcs:    
+    bar(r+i*bw, V[:,i], width=bw)
+xticks(r+bw, attrnames)
+xlabel('Attributes')
+ylabel('Component coefficients')
+legend(legendStrs)
+grid()
+title('AnAge: PCA Component Coefficients')
+show()
--- visual.py
+++ visual.py
@@ -1,6 +1,43 @@
 from dataload import *
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import (figure, subplot, boxplot, title, xticks, ylim, 
+                               show, plot, xlabel, ylabel, yticks, legend)
 
-plt.boxplot(np.log(X))
-plt.show()
+#plt.boxplot(np.log(X))
+#plt.show()
+figure(figsize=(14,7))
+for c in range(C):
+    subplot(1,C,c+1)
+    class_mask = (y==c) # binary mask to extract elements of class c
+    # or: class_mask = nonzero(y==c)[0].tolist()[0] # indices of class c
+    
+    boxplot(np.log(X[class_mask,:]))
+    #title('Class: {0}'.format(classNames[c]))
+    title('Class: '+classNames[c])
+    xticks(range(1,len(attrnames)+1), [a[:7] for a in attrnames], rotation=45)
+    y_up = X.max()+(X.max()-X.min())*0.1; y_down = X.min()-(X.max()-X.min())*0.1
+    #ylim(np.log(y_down), np.log(y_up))
+
+show()
+
+# Correlation
+figure(figsize=(12,10))
+for m1 in range(M):
+    for m2 in range(M):
+        subplot(M, M, m1*M + m2 + 1)
+        for c in range(C):
+            class_mask = (y==c)
+            plot(np.array(X[class_mask,m2]), np.array(X[class_mask,m1]), '.')
+            if m1==M-1:
+                xlabel(attrnames[m2])
+            else:
+                xticks([])
+            if m2==0:
+                ylabel(attrnames[m1])
+            else:
+                yticks([])
+            #ylim(0,X.max()*1.1)
+            #xlim(0,X.max()*1.1)
+legend(classNames)
+
+show()
