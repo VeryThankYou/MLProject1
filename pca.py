1bdf4b87d6a6e71928e547b65e13109abfd8a970
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2022-10-02 15:49:49 +0200

    dataload

---
+++ pca.py
@@ -0,0 +1,28 @@
+import dataload as dl
+from scipy.linalg import svd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Subtract mean value from data
+Y = dl.X - np.ones((N,1))*dl.X.mean(axis=0)
+
+# PCA by computing SVD of Y
+U,S,V = svd(Y,full_matrices=False)
+
+# Compute variance explained by principal components
+rho = (S*S) / (S*S).sum() 
+
+threshold = 0.9
+
+# Plot variance explained
+plt.figure()
+plt.plot(range(1,len(rho)+1),rho,'x-')
+plt.plot(range(1,len(rho)+1),np.cumsum(rho),'o-')
+plt.plot([1,len(rho)],[threshold, threshold],'k--')
+plt.title('Variance explained by principal components');
+plt.xlabel('Principal component');
+plt.ylabel('Variance explained');
+plt.legend(['Individual','Cumulative','Threshold'])
+plt.grid()
+plt.show()
