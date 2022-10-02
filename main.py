7aa2f7fab3997fa104cc192d8dfda958e1ab8524
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2022-10-02 15:24:05 +0200

    labels

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
+from cProfile import label
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 pddata = pd.read_csv("anage.csv")
-l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 15, 17, 19, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+l = [1, 2, 4, 5, 6, 7, 8, 12, 15, 17, 19, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 l.reverse()
 for e in l:
     pddata = pddata.drop(pddata.columns[e], axis=1)
-print(pddata)
-print(pddata.isnull().sum())
 
 
-
+labels = pddata[["HAGRID", "Class"]].to_numpy()
+pddata = pddata.drop(pddata.columns[1], axis=1)
 X = pddata.to_numpy()
-
-
-# print(pddata.isnull().sum())
-# print(type(X[0, 0]))
-# print(X[0, 0])
 X = X[~np.isnan(X).any(axis = 1)]
-print(X.shape)
+idlist = []
+for i, e in enumerate(labels):
+    exists = False
+    for e2 in X:
+        if e[0] == int(e2[0]):
+            exists = True
+            break
+    if exists == False:
+        idlist.append(i)
+labels = np.delete(labels, idlist, 0)
+
+acount = 0
+mcount = 0
+for e in labels:
+    if e[1] == "Aves":
+        acount = acount + 1
+    else:
+        mcount = mcount + 1
+
+print(acount, mcount)
+
