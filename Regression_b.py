4979c50ae819c77f0fcf7a3da4e0fa9967091769
jesp9435 <jesp9435@gmail.com>
2022-11-13 16:55:40 +0100

    .

--- Regression_b.py
+++ Regression_b.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import loadmat
 from sklearn import model_selection
-from extrascripts import train_neural_net, draw_neural_net, visualize_decision_boundary
+from extrascripts import feature_selector_lr, train_neural_net, draw_neural_net, visualize_decision_boundary
 import torch
 from dataload import *
+import sklearn.linear_model as lm
 plt.rcParams.update({'font.size': 12})
 
 
@@ -19,24 +20,57 @@ N, M = X.shape
 
 # K-fold CrossValidation
 K = 10
-CV = model_selection.KFold(n_splits=K,shuffle=True)
+CV_out = model_selection.KFold(n_splits=K,shuffle=True)
 
-# Setup figure for display of the decision boundary for the several crossvalidation folds.
-decision_boundaries = plt.figure(1, figsize=(10,10))
-# Determine a size of a plot grid that fits visualizations for the chosen number
-# of cross-validation splits, if K=4, this is simply a 2-by-2 grid.
-subplot_size_1 = int(np.floor(np.sqrt(K))) 
-subplot_size_2 = int(np.ceil(K/subplot_size_1))
-# Set overall title for all of the subplots
-plt.suptitle('Data and model decision boundaries', fontsize=20)
-# Change spacing of subplots
-plt.subplots_adjust(left=0, bottom=0, right=1, top=.9, wspace=.5, hspace=0.25)
+outer_results = list()
 
-# Setup figure for display of learning curves and error rates in fold
-summaries, summaries_axes = plt.subplots(1, 2, figsize=(10,5))
-# Make a list for storing assigned color of learning curve for up to K=10
-color_list = ['tab:orange', 'tab:green', 'tab:purple', 'tab:brown', 'tab:pink',
-              'tab:gray', 'tab:olive', 'tab:cyan', 'tab:red', 'tab:blue']
+for train_ix, test_ix in CV_out.split(X):
+    # split data 
+    X_train, X_test = X[train_ix, :], X[test_ix, :]
+    y_train, y_test = y[train_ix], y[test_ix]
+	# configure the cross-validation procedure 
+    CV_inner = model_selection.KFold(n_splits=10, shuffle=True)
+
+    for k, (train_index, test_index) in enumerate(CV_inner.split(X,y)):
+        print('\nCrossvalidation fold: {0}/{1}'.format(k+1,K))   
+    
+        # define the model
+        # Extract training and test set for current CV fold, 
+        # and convert them to PyTorch tensors
+        X_train = torch.Tensor(X[train_index,:] )
+        y_train = torch.Tensor(y[train_index] )
+        X_test = torch.Tensor(X[test_index,:] )
+        y_test = torch.Tensor(y[test_index] )
+        y_train = y_train.reshape(-1, 1)
+        # Go to the file 'toolbox_02450.py' in the Tools sub-folder of the toolbox
+        # and see how the network is trained (search for 'def train_neural_net',
+        # which is the place the function below is defined)
+        net, final_loss, learning_curve = train_neural_net(model,
+                                                        loss_fn,
+                                                        X=X_train,
+                                                        y=y_train,
+                                                        n_replicates=3,
+                                                        max_iter=max_iter)
+        
+        print('\n\tBest loss: {}\n'.format(final_loss))
+        
+        # Determine estimated class labels for test set
+        y_sigmoid = net(X_test) # activation of final note, i.e. prediction of network
+        y_test_est = (y_sigmoid > .5).type(dtype=torch.uint8) # threshold output of sigmoidal function
+        y_test = y_test.type(dtype=torch.uint8)
+        # Determine errors and error rate
+        e = (y_test_est != y_test)
+        error_rate = (sum(e).type(torch.float)/len(y_test)).data.numpy()
+        errors.append(error_rate) # store error rate for current CV fold 
+
+
+
+
+
+
+
+
+
 
 # Define the model structure
 n_hidden_units = 1 # number of hidden units in the signle hidden layer
@@ -64,79 +98,59 @@ errors = [] # make a list for storing generalizaition error in each loop
 # indices to be used for training and testing in each split, and calling 
 # the enumerate-method with this simply returns this indices along with 
 # a counter k:
-for k, (train_index, test_index) in enumerate(CV.split(X,y)): 
-    print('\nCrossvalidation fold: {0}/{1}'.format(k+1,K))    
-    
-    # Extract training and test set for current CV fold, 
-    # and convert them to PyTorch tensors
-    X_train = torch.Tensor(X[train_index,:] )
-    y_train = torch.Tensor(y[train_index] )
-    X_test = torch.Tensor(X[test_index,:] )
-    y_test = torch.Tensor(y[test_index] )
-    y_train = y_train.reshape(-1, 1)
-    # Go to the file 'toolbox_02450.py' in the Tools sub-folder of the toolbox
-    # and see how the network is trained (search for 'def train_neural_net',
-    # which is the place the function below is defined)
-    net, final_loss, learning_curve = train_neural_net(model,
-                                                       loss_fn,
-                                                       X=X_train,
-                                                       y=y_train,
-                                                       n_replicates=3,
-                                                       max_iter=max_iter)
-    
-    print('\n\tBest loss: {}\n'.format(final_loss))
-    
-    # Determine estimated class labels for test set
-    y_sigmoid = net(X_test) # activation of final note, i.e. prediction of network
-    y_test_est = (y_sigmoid > .5).type(dtype=torch.uint8) # threshold output of sigmoidal function
-    y_test = y_test.type(dtype=torch.uint8)
-    # Determine errors and error rate
-    e = (y_test_est != y_test)
-    error_rate = (sum(e).type(torch.float)/len(y_test)).data.numpy()
-    errors.append(error_rate) # store error rate for current CV fold 
-    
-    # Make a subplot for current cross validation fold that displays the 
-    # decision boundary over the original data, "background color" corresponds
-    # to the output of the sigmoidal transfer function (i.e. before threshold),
-    # white areas are areas of uncertainty, and a deaper red/blue means 
-    # that the network "is more sure" of a given class.
-    plt.figure(decision_boundaries.number)
-    plt.subplot(subplot_size_1,subplot_size_2,k+1)
-    plt.title('CV fold {0}'.format(k+1),color=color_list[k])
-    predict = lambda x: net(torch.tensor(x, dtype=torch.float)).data.numpy()
-    visualize_decision_boundary(predict, X, y, # provide data, along with function for prediction
-                                attributeNames, classNames, # provide information on attribute and class names
-                                train=train_index, test=test_index, # provide information on partioning
-                                show_legend=k==(K-1)) # only display legend for last plot
-    
-    # Display the learning curve for the best net in the current fold
-    h, = summaries_axes[0].plot(learning_curve, color=color_list[k])
-    h.set_label('CV fold {0}'.format(k+1))
-    summaries_axes[0].set_xlabel('Iterations')
-    summaries_axes[0].set_xlim((0, max_iter))
-    summaries_axes[0].set_ylabel('Loss')
-    summaries_axes[0].set_title('Learning curves')
-    
-# Display the error rate across folds
-summaries_axes[1].bar(np.arange(1, K+1), np.squeeze(np.asarray(errors)), color=color_list)
-summaries_axes[1].set_xlabel('Fold');
-summaries_axes[1].set_xticks(np.arange(1, K+1))
-summaries_axes[1].set_ylabel('Error rate');
-summaries_axes[1].set_title('Test misclassification rates')
-    
-# Show the plots
-# plt.show(decision_boundaries.number) # try these lines if the following code fails (depends on package versions)
-# plt.show(summaries.number)
-plt.show()
 
-# Display a diagram of the best network in last fold
-print('Diagram of best neural net in last fold:')
-weights = [net[i].weight.data.numpy().T for i in [0,2]]
-biases = [net[i].bias.data.numpy() for i in [0,2]]
-tf =  [str(net[i]) for i in [1,3]]
-draw_neural_net(weights, biases, tf)
+# Initialize variables
+Features = np.zeros((M,K))
+Error_train = np.empty((K,1))
+Error_test = np.empty((K,1))
+Error_train_fs = np.empty((K,1))
+Error_test_fs = np.empty((K,1))
+Error_train_nofeatures = np.empty((K,1))
+Error_test_nofeatures = np.empty((K,1))
 
-# Print the average classification error rate
-print('\nGeneralization error/average error rate: {0}%'.format(round(100*np.mean(errors),4)))
 
-print('Ran exercise 8.2.2.')
+
+ 
+    
+    
+    
+
+    
+    
+#     # visualize_decision_boundary(predict, X, y, # provide data, along with function for prediction
+#     #                             attrnames, classNames, # provide information on attribute and class names
+#     #                             train=train_index, test=test_index, # provide information on partioning
+#     #                             show_legend=k==(K-1)) # only display legend for last plot
+    
+    
+#     # Display the learning curve for the best net in the current fold
+#     h, = summaries_axes[0].plot(learning_curve, color=color_list[k])
+#     h.set_label('CV fold {0}'.format(k+1))
+#     summaries_axes[0].set_xlabel('Iterations')
+#     summaries_axes[0].set_xlim((0, max_iter))
+#     summaries_axes[0].set_ylabel('Loss')
+#     summaries_axes[0].set_title('Learning curves')
+    
+# # Display the error rate across folds
+# summaries_axes[1].bar(np.arange(1, K+1), np.squeeze(np.asarray(errors)), color=color_list)
+# summaries_axes[1].set_xlabel('Fold')
+# summaries_axes[1].set_xticks(np.arange(1, K+1))
+# summaries_axes[1].set_ylabel('Error rate')
+# summaries_axes[1].set_title('Test misclassification rates')
+    
+# # Show the plots
+# # plt.show(decision_boundaries.number) # try these lines if the following code fails (depends on package versions)
+# # plt.show(summaries.number)
+# plt.show()
+
+# # Display a diagram of the best network in last fold
+# print('Diagram of best neural net in last fold:')
+# weights = [net[i].weight.data.numpy().T for i in [0,2]]
+# biases = [net[i].bias.data.numpy() for i in [0,2]]
+# tf =  [str(net[i]) for i in [1,3]]
+# draw_neural_net(weights, biases, tf)
+
+# # Print the average classification error rate
+# print('\nGeneralization error/average error rate: {0}%'.format(round(100*np.mean(errors),4)))
+
+# print('Ran exercise 8.2.2.')
