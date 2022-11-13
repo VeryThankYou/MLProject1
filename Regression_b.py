781f3fdb6c0fe7358324c82c2075e19fc0ef6266
jesp9435 <jesp9435@gmail.com>
2022-11-13 15:14:34 +0100

    f

--- Regression_b.py
+++ Regression_b.py
@@ -1,128 +1,142 @@
-from dataload import *
-from matplotlib.pyplot import figure, plot, subplot, title, xlabel, ylabel, show, clim
-from scipy.io import loadmat
-import sklearn.linear_model as lm
-from sklearn import model_selection
-from extrascripts import feature_selector_lr, bmplot
+import matplotlib.pyplot as plt
 import numpy as np
+from scipy.io import loadmat
+from sklearn import model_selection
+from extrascripts import train_neural_net, draw_neural_net, visualize_decision_boundary
+import torch
+from dataload import *
+plt.rcParams.update({'font.size': 12})
 
-# Load data from matlab file
-mat_data = loadmat('Data/wine2.mat')
-X = mat_data['X']
-y = mat_data['y'].squeeze()
-attributeNames = [name[0] for name in mat_data['attributeNames'][0]]
+
+# Split dataset into features and target vector
+life_idx = attrnames.index('Maximum longevity (yrs)')
+y = X[:,life_idx]
+attrnames.pop(7)
+X_cols = list(range(0,life_idx)) + list(range(life_idx+1,len(attrnames)))
+X = X[:,X_cols]
+
 N, M = X.shape
 
-
-## Crossvalidation
-# Create crossvalidation partition for evaluation
-K = 5
+# K-fold CrossValidation
+K = 10
 CV = model_selection.KFold(n_splits=K,shuffle=True)
 
-# Initialize variables
-Features = np.zeros((M,K))
-Error_train = np.empty((K,1))
-Error_test = np.empty((K,1))
-Error_train_fs = np.empty((K,1))
-Error_test_fs = np.empty((K,1))
-Error_train_nofeatures = np.empty((K,1))
-Error_test_nofeatures = np.empty((K,1))
+# Setup figure for display of the decision boundary for the several crossvalidation folds.
+decision_boundaries = plt.figure(1, figsize=(10,10))
+# Determine a size of a plot grid that fits visualizations for the chosen number
+# of cross-validation splits, if K=4, this is simply a 2-by-2 grid.
+subplot_size_1 = int(np.floor(np.sqrt(K))) 
+subplot_size_2 = int(np.ceil(K/subplot_size_1))
+# Set overall title for all of the subplots
+plt.suptitle('Data and model decision boundaries', fontsize=20)
+# Change spacing of subplots
+plt.subplots_adjust(left=0, bottom=0, right=1, top=.9, wspace=.5, hspace=0.25)
 
-k=0
-for train_index, test_index in CV.split(X):
+# Setup figure for display of learning curves and error rates in fold
+summaries, summaries_axes = plt.subplots(1, 2, figsize=(10,5))
+# Make a list for storing assigned color of learning curve for up to K=10
+color_list = ['tab:orange', 'tab:green', 'tab:purple', 'tab:brown', 'tab:pink',
+              'tab:gray', 'tab:olive', 'tab:cyan', 'tab:red', 'tab:blue']
+
+# Define the model structure
+n_hidden_units = 1 # number of hidden units in the signle hidden layer
+# The lambda-syntax defines an anonymous function, which is used here to 
+# make it easy to make new networks within each cross validation fold
+model = lambda: torch.nn.Sequential(
+                    torch.nn.Linear(M, n_hidden_units), #M features to H hiden units
+                    # 1st transfer function, either Tanh or ReLU:
+                    torch.nn.Tanh(),                            #torch.nn.ReLU(),
+                    torch.nn.Linear(n_hidden_units, 1), # H hidden units to 1 output neuron
+                    torch.nn.Sigmoid() # final tranfer function
+                    )
+# Since we're training a neural network for binary classification, we use a 
+# binary cross entropy loss (see the help(train_neural_net) for more on
+# the loss_fn input to the function)
+loss_fn = torch.nn.BCELoss()
+# Train for a maximum of 10000 steps, or until convergence (see help for the 
+# function train_neural_net() for more on the tolerance/convergence))
+max_iter = 10000
+print('Training model of type:\n{}\n'.format(str(model())))
+
+# Do cross-validation:
+errors = [] # make a list for storing generalizaition error in each loop
+# Loop over each cross-validation split. The CV.split-method returns the 
+# indices to be used for training and testing in each split, and calling 
+# the enumerate-method with this simply returns this indices along with 
+# a counter k:
+for k, (train_index, test_index) in enumerate(CV.split(X,y)): 
+    print('\nCrossvalidation fold: {0}/{1}'.format(k+1,K))    
     
-    # extract training and test set for current CV fold
-    X_train = X[train_index,:]
-    y_train = y[train_index]
-    X_test = X[test_index,:]
-    y_test = y[test_index]
-    internal_cross_validation = 10
+    # Extract training and test set for current CV fold, 
+    # and convert them to PyTorch tensors
+    X_train = torch.Tensor(X[train_index,:] )
+    y_train = torch.Tensor(y[train_index] )
+    X_test = torch.Tensor(X[test_index,:] )
+    y_test = torch.Tensor(y[test_index] )
     
-    # Compute squared error without using the input data at all
-    Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum()/y_train.shape[0]
-    Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum()/y_test.shape[0]
-
-    # Compute squared error with all features selected (no feature selection)
-    m = lm.LinearRegression(fit_intercept=True).fit(X_train, y_train)
-    Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
-    Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
-
-    # Compute squared error with feature subset selection
-    textout = ''
-    selected_features, features_record, loss_record = feature_selector_lr(X_train, y_train, internal_cross_validation,display=textout)
+    # Go to the file 'toolbox_02450.py' in the Tools sub-folder of the toolbox
+    # and see how the network is trained (search for 'def train_neural_net',
+    # which is the place the function below is defined)
+    net, final_loss, learning_curve = train_neural_net(model,
+                                                       loss_fn,
+                                                       X=X_train,
+                                                       y=y_train,
+                                                       n_replicates=3,
+                                                       max_iter=max_iter)
     
-    Features[selected_features,k] = 1
-    # .. alternatively you could use module sklearn.feature_selection
-    if len(selected_features) == 0:
-        print('No features were selected, i.e. the data (X) in the fold cannot describe the outcomes (y).' )
-    else:
-        m = lm.LinearRegression(fit_intercept=True).fit(X_train[:,selected_features], y_train)
-        Error_train_fs[k] = np.square(y_train-m.predict(X_train[:,selected_features])).sum()/y_train.shape[0]
-        Error_test_fs[k] = np.square(y_test-m.predict(X_test[:,selected_features])).sum()/y_test.shape[0]
+    print('\n\tBest loss: {}\n'.format(final_loss))
     
-        figure(k)
-        subplot(1,2,1)
-        plot(range(1,len(loss_record)), loss_record[1:])
-        xlabel('Iteration')
-        ylabel('Squared error (crossvalidation)')    
-        
-        subplot(1,3,3)
-        bmplot(attributeNames, range(1,features_record.shape[1]), -features_record[:,1:])
-        clim(-1.5,0)
-        xlabel('Iteration')
-
-    print('Cross validation fold {0}/{1}'.format(k+1,K))
-    print('Train indices: {0}'.format(train_index))
-    print('Test indices: {0}'.format(test_index))
-    print('Features no: {0}\n'.format(selected_features.size))
-
-    k+=1
-
-
-# Display results
-print('\n')
-print('Linear regression without feature selection:\n')
-print('- Training error: {0}'.format(Error_train.mean()))
-print('- Test error:     {0}'.format(Error_test.mean()))
-print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train.sum())/Error_train_nofeatures.sum()))
-print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test.sum())/Error_test_nofeatures.sum()))
-print('Linear regression with feature selection:\n')
-print('- Training error: {0}'.format(Error_train_fs.mean()))
-print('- Test error:     {0}'.format(Error_test_fs.mean()))
-print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_fs.sum())/Error_train_nofeatures.sum()))
-print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test_fs.sum())/Error_test_nofeatures.sum()))
-
-figure(k)
-subplot(1,3,2)
-bmplot(attributeNames, range(1,Features.shape[1]+1), -Features)
-clim(-1.5,0)
-xlabel('Crossvalidation fold')
-ylabel('Attribute')
-
-
-# Inspect selected feature coefficients effect on the entire dataset and
-# plot the fitted model residual error as function of each attribute to
-# inspect for systematic structure in the residual
-
-f=2 # cross-validation fold to inspect
-ff=Features[:,f-1].nonzero()[0]
-if len(ff) == 0:
-    print('\nNo features were selected, i.e. the data (X) in the fold cannot describe the outcomes (y).' )
-else:
-    m = lm.LinearRegression(fit_intercept=True).fit(X[:,ff], y)
+    # Determine estimated class labels for test set
+    y_sigmoid = net(X_test) # activation of final note, i.e. prediction of network
+    y_test_est = (y_sigmoid > .5).type(dtype=torch.uint8) # threshold output of sigmoidal function
+    y_test = y_test.type(dtype=torch.uint8)
+    # Determine errors and error rate
+    e = (y_test_est != y_test)
+    error_rate = (sum(e).type(torch.float)/len(y_test)).data.numpy()
+    errors.append(error_rate) # store error rate for current CV fold 
     
-    y_est= m.predict(X[:,ff])
-    residual=y-y_est
+    # Make a subplot for current cross validation fold that displays the 
+    # decision boundary over the original data, "background color" corresponds
+    # to the output of the sigmoidal transfer function (i.e. before threshold),
+    # white areas are areas of uncertainty, and a deaper red/blue means 
+    # that the network "is more sure" of a given class.
+    plt.figure(decision_boundaries.number)
+    plt.subplot(subplot_size_1,subplot_size_2,k+1)
+    plt.title('CV fold {0}'.format(k+1),color=color_list[k])
+    predict = lambda x: net(torch.tensor(x, dtype=torch.float)).data.numpy()
+    visualize_decision_boundary(predict, X, y, # provide data, along with function for prediction
+                                attributeNames, classNames, # provide information on attribute and class names
+                                train=train_index, test=test_index, # provide information on partioning
+                                show_legend=k==(K-1)) # only display legend for last plot
     
-    figure(k+1, figsize=(12,6))
-    title('Residual error vs. Attributes for features selected in cross-validation fold {0}'.format(f))
-    for i in range(0,len(ff)):
-       subplot(2, int( np.ceil(len(ff) // 2)),i+1)
-       plot(X[:,ff[i]],residual,'.')
-       xlabel(attributeNames[ff[i]])
-       ylabel('residual error')
+    # Display the learning curve for the best net in the current fold
+    h, = summaries_axes[0].plot(learning_curve, color=color_list[k])
+    h.set_label('CV fold {0}'.format(k+1))
+    summaries_axes[0].set_xlabel('Iterations')
+    summaries_axes[0].set_xlim((0, max_iter))
+    summaries_axes[0].set_ylabel('Loss')
+    summaries_axes[0].set_title('Learning curves')
     
+# Display the error rate across folds
+summaries_axes[1].bar(np.arange(1, K+1), np.squeeze(np.asarray(errors)), color=color_list)
+summaries_axes[1].set_xlabel('Fold');
+summaries_axes[1].set_xticks(np.arange(1, K+1))
+summaries_axes[1].set_ylabel('Error rate');
+summaries_axes[1].set_title('Test misclassification rates')
     
-show()
+# Show the plots
+# plt.show(decision_boundaries.number) # try these lines if the following code fails (depends on package versions)
+# plt.show(summaries.number)
+plt.show()
 
-print('Ran Exercise 6.2.1')
+# Display a diagram of the best network in last fold
+print('Diagram of best neural net in last fold:')
+weights = [net[i].weight.data.numpy().T for i in [0,2]]
+biases = [net[i].bias.data.numpy() for i in [0,2]]
+tf =  [str(net[i]) for i in [1,3]]
+draw_neural_net(weights, biases, tf)
+
+# Print the average classification error rate
+print('\nGeneralization error/average error rate: {0}%'.format(round(100*np.mean(errors),4)))
+
+print('Ran exercise 8.2.2.')
